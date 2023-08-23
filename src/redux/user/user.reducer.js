286c7e537b559018f49edb695bd06ee58f414be7
Jerry Jiang <jerryjiang49@gmail.com>
2023-08-23 19:55:58 -0400

    WIP: add boilerplate user reducer code

---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,14 @@
+// BOILERPLATE CODE, EDIT THIS
+
+export const INITIAL_USER_STATE = {
+    user: {}
+};
+
+const userReducer = (state = INITIAL_USER_STATE, { type, payload }) => {
+    switch (type) {
+        default:
+            return state;
+    }
+};
+
+export default userReducer;
