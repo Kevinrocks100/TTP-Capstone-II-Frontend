2c8e400004e8d19c7f51f12bc6f8c261d318b9ea
Kevin Liu <you@example.com>
2023-08-28 18:32:16 -0400

    feat!: set up login status in react state

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,30 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { useSelector, useDispatch } from 'react-redux';
+import { setLoggedIn, setLoggedOut } from '../redux/auth/auth.actions';
 import Home from "../pages";
 import RepoListPage from "../pages/repo/list/RepoListPage";
 import RepoMetricPage from "../pages/repo/metrics/RepoMetricPage";
 import Error404 from "../pages/Error404";
 
 function App() {
+    const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+    const dispatch = useDispatch();
+
+    const handleLogin = () => {
+        dispatch(setLoggedIn());
+    };
+
+    const handleLogout = () => {
+        dispatch(setLoggedOut());
+    };
+
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Home/>}/>
+                <Route path="/" element={<Home handleLogin = {handleLogin}/>}/>
                 {/*PROTECTED USER ROUTES */}
                 <Route path="/repos" element={<RepoListPage/>}/>
-                <Route path={"/repos/:repoId"} element={<RepoMetricPage />} />
+                <Route path={"/repos/:repoId"} element={<RepoMetricPage handleLogout = {handleLogout}/>} />
                 <Route path="*" element={<Error404 />} />
             </Routes>
         </Router>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,14 @@ import GithubOAuthButton from '../components/githubOAuthButton';
 import useDocumentTitle from "../components/useDocumentTitle";
 import { initializeApp } from "firebase/app";
 
-function Home() {
+function Home({handleLogin}) {
     useDocumentTitle("GitMe");
     return (
         <div className={styles.main}>
             <div className={styles.container}>
                 <h1 className={styles.container__header}>GitMe</h1>
                 <span className={styles.container__header__text}>Every metric you need for your repository</span>
-                <GithubOAuthButton />
+                <GithubOAuthButton handleLogin = {handleLogin}/>
             </div>
         </div>
     )
