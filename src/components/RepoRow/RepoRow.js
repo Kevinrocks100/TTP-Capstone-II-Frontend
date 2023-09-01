02abb575d8a48cdf15702da3168c5cc0303df1be
Jerry Jiang <jerryjiang49@gmail.com>
2023-08-31 22:34:12 -0400

    feat: add logout button
    1) updated navbar styling
    2) add logout button functionality

--- src/components/RepoRow/RepoRow.js
+++ src/components/RepoRow/RepoRow.js
@@ -2,12 +2,13 @@ import styles from './RepoRow.module.scss'
 import arrow from '../../assets/arrow.jpg'
 
 export default function RepoRow({fullName, id, handleRepoClick}) {
+
     return (
         <div id={id} className={styles.repoRow}>
             <div id = {id} className={styles.repoRow__name}>
                 <span>{fullName}</span>
             </div>
-            <button id = {id} onClick={handleRepoClick}>
+            <button id={id} onClick={handleRepoClick}>
                 <img className={styles.repoRow__arrow} src={arrow} alt="Arrow"/>
             </button>
         </div>
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,15 +1,31 @@
 import styles from './Navbar.module.scss'
 import githubLogo from '../../assets/github-alt-logo.png'
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import {useDispatch} from "react-redux";
+import {setLoggedOut} from "../../redux/auth/auth.actions";
+import exitIcon from "../../assets/exit.png";
 
 export default function Navbar() {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleLogoutHandler = () => {
+        // Logout user and redirects to home page
+        dispatch(setLoggedOut());
+        navigate('/');
+    }
+
     return (
         <div className={styles.navbar}>
-            <Link to="/repos">
-                <img className={styles.navbar__logo} src={githubLogo} alt="Github Logo"/>
+            <Link className={styles.navbar__link} to="/repos">
+                <img className={styles.navbar__link__logo} src={githubLogo} alt="Github Logo"/>
+                <span className={styles.navbar__link__name}>GitMe</span>
             </Link>
             <div>
-                {/*insert github username here*/}
+                <button onClick={handleLogoutHandler} className={styles.navbar__button}>
+                    <img style={{marginRight: ".5rem"}} src={exitIcon} alt="logout icon"/>
+                    <span>Logout</span>
+                </button>
             </div>
         </div>
     )
